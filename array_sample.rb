5b5e3ff5f550d84ddf0eda40daff728600acd1cd
DoroYoshi <yoshimatsu44dy@gmail.com>
2019-12-07 14:25:27 +0900

    1 case文を使っての条件分岐へ変更。 2 共通部分を変数に格納した。

--- array_sample.rb
+++ array_sample.rb
@@ -1,12 +1,14 @@
 puts "様々な言語のHello World"
 languages = ["Ruby", "PHP", "Java"]
+greeting = "\"Hello World!\""
 
 languages.each do |language|
-  if language == "Ruby"
-    puts "#{language}:puts \"Hello World!\""
-  elsif language == "PHP"
-    puts "#{language}:echo \"Hello World!\";"
-  else
-    puts "#{language}:System.out.println(\"Hello World!\");"
+  case language
+  when "Ruby"
+    puts "#{language}:puts #{greeting}"
+  when "PHP"
+    puts "#{language}:echo #{greeting};"
+  when "Java"
+    puts "#{language}:System.out.println(#{greeting});"
   end
 end
